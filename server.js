9a26d483f9d4194a75e74fd798d97c160c3d92b6
Glitch (discord-bot-example) <none>
2024-01-17 16:00:38 +0000

    🐬⛽️ Checkpoint ./server.js:56257826/676 ./.gitignore:56257826/18

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ if (msg.content.includes('@bot1197118174164951100 ')) {msg.content.substring('@b
 const command = msg.content.substring(6)
 const output = eval(command)
 bot.createMessage(msg.channel.id, '**Eval Output**\n```'+output+'```')
+} else if (msg.content.startsWith('/users')) {
+bot.createMessage(msg.channel.id, '**USERS KNOWN**\n```1140485340977692747 - @sh4d0wl0rd3_52746 AKA Ralsei/MLG\n741024771034317001 - @itsyeboitai aka ||token leaker||\n1190727236911910963 - @antivanguard\n406204826003963904 - @umm yea idk 🤯```\n')
 }
 });
  
