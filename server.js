9f4d35cec7df8922e133746fe636888462b13645
Glitch (discord-bot-example) <none>
2024-01-18 14:23:41 +0000

    🕋🍉 Checkpoint ./server.js:56257826/502

--- server.js
+++ server.js
@@ -24,18 +24,7 @@ const output2 = eval(command)
       text: 'Requested by ' + msg.author.username,
     },
   };
-  const embed2 = {
-    title: 'Eval Output',
-    color: 0x7289DA, //green: 0x00FF00, red: 0xFF0000
-    fields: [
-      { name: '```'+command+'```'},
-    ],
-    footer: {
-      text: 'Requested by ' + msg.author.username,
-    },
-  };
-  if ()
-  bot.createMessage(msg.channel.id, { embed });
+ bot.createMessage(msg.channel.id, { embed });
 //bot.createMessage(msg.channel.id, '**Eval Output**\n```'+output+'```')
 } else if (msg.content.startsWith('/users')) {
 bot.createMessage(msg.channel.id, '**USERS KNOWN**\n```1140485340977692747 - @sh4d0wl0rd3_52746 AKA Ralsei/MLG\n741024771034317001 - @itsyeboitai aka token leaker\n1190727236911910963 - @antivanguard\n406204826003963904 - @umm yea idk 🤯```\nRequested by ' + msg.author)
