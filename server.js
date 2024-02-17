994cd139c1ebe0d72aa3de9054937ca85095c238
Glitch (rose-horse-freighter-bot) <none>
2024-02-17 08:26:29 +0000

    💾🚦 Checkpoint ./server.js:56257826/354

--- server.js
+++ server.js
@@ -60,10 +60,6 @@ if (msg.content.includes('/servers')) {
 };
 bot.createMessage(msg.channel.id, { embed });
 //bot.createMessage(msg.channel.id, '18 servers:\n```rose-horse-freighter.glitch.me/#us, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#lt, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#wxc, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#x, hidden: '+ true + ', ended: ' + true + ', event\nrose-horse-freighter.glitch.me/#o, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#c, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#af, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#az, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#wz, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#sz, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#wf, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#sf, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#au, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#wu, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#su, hidden: '+ true + ',\nrose-horse-freighter.glitch.me/#lm, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#sk, hidden: '+ false + ',\nrose-horse-freighter.glitch.me/#wm, hidden: ' + true + '```');
-} else if (msg.content.startsWith('Hello')) {
-bot.createMessage(msg.channel.id, 'Hi, <@' + msg.author.id + '>! 😄')
-} else if (msg.content.startsWith('Hi')) {
-bot.createMessage(msg.channel.id, 'Hello, <@' + msg.author.id + '>! 😄')
 } else if (msg.content.includes('/messagelogs')) {
 if (msg.author.id !== '1193882484727885884') {
   const embed = {
