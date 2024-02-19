e3dc7a0affea874c24d5d943c8dc62b7ad3a9e64
Esclippy990 <153932585+Esclippy990@users.noreply.github.com>
2024-02-19 16:43:31 +0100

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write("I'm alive");
+  res.write(`<font size="6"><center><b>Discord bot controls</b></center></font><br><input type="text" placeholder="Channel ID" id="channelID"><br><br><input type="text" placeholder="Send a message.." id="message"><button id="send-message">Send</button><button id="clear-message">Clear</button><script src="server.js"></script>`);
   res.end();
 }).listen(8080);
